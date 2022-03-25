ccb3559d32bd664cda18e58a59182ee31b98c39c
adjutantee <romashilov3233@gmail.com>
2022-03-25 15:24:11 +0300

    Deleting extra lines

--- OpenXMLExcel/Program.cs
+++ OpenXMLExcel/Program.cs
@@ -38,37 +38,6 @@ public class Program
                         }
                     }
                 }
-
-                //foreach (Cell cell in cells)
-                //{
-                //    if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
-                //    {
-                //        int ssid = int.Parse(cell.CellValue.Text);
-                //        string str = sst.ChildElements[ssid].InnerText;
-                //        Console.WriteLine("Shared string {0}: {1}", ssid, str);
-                //    }
-                //    else if (cell.CellValue != null)
-                //    {
-                //        Console.WriteLine("Cell contents: {0}", cell.CellValue.Text);
-                //    }
-                //}
-
-                //foreach (Row row in rows)
-                //{
-                //    foreach (Cell c in row.Elements<Cell>())
-                //    {
-                //        if ((c.DataType != null) && (c.DataType == CellValues.SharedString))
-                //        {
-                //            int ssid = int.Parse(c.CellValue.Text);
-                //            string str = sst.ChildElements[ssid].InnerText;
-                //            Console.WriteLine("Shared string {0}: {1}", ssid, str);
-                //        }
-                //        else if (c.CellValue != null)
-                //        {
-                //            Console.WriteLine("Cell contents: {0}", c.CellValue.Text);
-                //        }
-                //    }
-                //}
             }
         }
     }
