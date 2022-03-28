d58f398e900d2728edead8aaf45e20cf8ddb9e56
adjutantee <romashilov3233@gmail.com>
2022-03-28 12:09:48 +0300

    add class

---
+++ OpenXMLExcel/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenXMLExcel
+{
+    public class Category
+    {
+        public Guid id;
+        public string name;
+        public string type;
+        public string format;
+        public string laung;
+    }
+}
--- OpenXMLExcel/Program.cs
+++ OpenXMLExcel/Program.cs
@@ -4,39 +4,69 @@ using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 
-public class Program
+
+namespace OpenXMLExcel
 {
-    static void Main(string[] args)
+    public class Program
     {
-        string fileName = @"C:\Users\Izagakhmaevra\Desktop\Excel\TestExel.xlsx";
-
-        using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+        static void Main(string[] args)
         {
-            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
+            string fileName = @"C:\Users\Izagakhmaevra\Desktop\Excel\TestExel.xlsx";
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
-                WorkbookPart workbookPart = doc.WorkbookPart;
-                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
-                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
-
-                SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
-                SharedStringTable sst = sstpart.SharedStringTable;
-
-                foreach (var row in sheetData.Elements<Row>())
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
                 {
-                    foreach (var cell in row.Elements<Cell>())
-                    {
-                        if (cell.DataType != null && cell.DataType == CellValues.SharedString)
-                        {
-                            int ssid = int.Parse(cell.CellValue.Text);
-                            string str = sst.ChildElements[ssid].InnerText;
+                    WorkbookPart workbookPart = doc.WorkbookPart;
+                    WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                    SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
 
-                            Console.WriteLine("Shared string {0}: {1}", ssid, str);
-                        }
-                        else
+                    SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                    SharedStringTable sst = sstpart.SharedStringTable;
+
+                    static Category GetCategory(SheetData sheetData, SharedStringTable sst)
+                    {
+                        foreach (var row in sheetData.Elements<Row>())
                         {
-                            Console.WriteLine("Shared string {0}: {1}", );
+                            foreach (var cell in row.Elements<Cell>())
+                            {
+                                if (cell.DataType != null && cell.DataType == CellValues.SharedString)
+                                {
+                                    int ssid = int.Parse(cell.CellValue.Text);
+                                    string str = sst.ChildElements[ssid].InnerText;
+
+                                    //Console.WriteLine("Shared string {0}: {1}", ssid, str);
+
+                                    Category category = new Category();
+
+                                    category.name = str;
+                                    category.type = str;
+                                    category.format = str;
+                                    category.laung = str;
+
+                                    return category;
+                                }
+                                //else
+                                //{
+                                //    //Console.WriteLine("Shared string {0}: {1}", );
+                                //}
+
+                                static void Print(Category category)
+                                {
+                                    Console.WriteLine($"ID: {category.id}");
+                                    Console.WriteLine($"Имя: {category.name}");
+                                    Console.WriteLine($"Тип файла: {category.type}");
+                                    Console.WriteLine($"Формат файла: {category.format}");
+                                    Console.WriteLine($"Язык: {category.laung}");
+                                }
+
+                                static void Additional(string[] args, Category firstCategory)
+                                {
+                                    Print(firstCategory);
+                                }
+                            }
                         }
-                    }
+                    }                   
                 }
             }
         }
